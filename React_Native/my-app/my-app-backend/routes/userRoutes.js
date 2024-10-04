acab8432dffbb809c1bea825ab6c8341cd46a0e4
balram16 <141282145+balram16@users.noreply.github.com>
2024-10-04 12:25:34 +0530

    Delete React_Native/my-app directory

--- React_Native/my-app/my-app-backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createUser } = require('../controllers/userController');
-
-const router = express.Router();
-
-// Route to create a user
-router.post('/create-user', createUser);
-
-module.exports = router;
--- React_Native/my-app/my-app-backend/utils/generateUniqueCode.js
+++
@@ -1,7 +0,0 @@
-// Generate Unique 5-digit Code
-const generateUniqueCode = () => {
-    return Math.floor(10000 + Math.random() * 90000);
-  };
-  
-  module.exports = generateUniqueCode;
-  
